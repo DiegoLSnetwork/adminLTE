93c39b768a75941825e267b5703602182df2b1a0
DiegoLSnetwork <diego.gutierrez@smartnetworksas.com>
2022-01-14 17:35:52 -0500

    Express server installed

---
+++ src/serve.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+//
+app.set("port", process.env.PORT || 3000);
+//Morgan
+app.use(morgan("dev"));
+
+module.exports= app;
